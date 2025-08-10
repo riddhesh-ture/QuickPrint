b4acc8d97e48b924c9edf8efc83f1cc132081549
Time_Traveller <terepapakamaal@proton.me>
2025-08-10 18:18:22 +0530

    firebase init

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJrAsrM9uG-IASLrhWURMaz5d4gEF44dA",
+  authDomain: "quickprint-official.firebaseapp.com",
+  projectId: "quickprint-official",
+  storageBucket: "quickprint-official.firebasestorage.app",
+  messagingSenderId: "876471482977",
+  appId: "1:876471482977:web:f65d816bbd8c8ab5a88820",
+  measurementId: "G-VT7W8B76G3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
